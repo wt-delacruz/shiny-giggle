c0935557973559f7b5331a2bbea793af95541424
Willy De la Cruz Sierra <will.delacruz.sierra@gmail.com>
2020-01-13 05:34:26 -0500

    removing adb start/stop as Appium offers it out of the box

--- src/main/java/org/fis/framework/service/MobileSettings.java
+++ src/main/java/org/fis/framework/service/MobileSettings.java
@@ -7,11 +7,9 @@ public class MobileSettings {
     final String DEVICE_DESCRIPTOR_DIR_PATH = "src/test/resources/device-descriptors";
     private String deviceDescriptorFileName;
     private DesiredCapabilities capabilities;
-    private AndroidEmulator androidEmulator;
 
     public MobileSettings(String deviceDescriptorFileName) {
         this.deviceDescriptorFileName = deviceDescriptorFileName;
-        this.androidEmulator = new AndroidEmulator();
     }
 
     public DesiredCapabilities getCapabilities() {
@@ -22,32 +20,5 @@ public class MobileSettings {
         this.capabilities = Capabilities.set(
                 this.DEVICE_DESCRIPTOR_DIR_PATH,
                 this.deviceDescriptorFileName);
-    }
-
-    public void startSimulator() {
-        String deviceName = this.capabilities.getCapability("deviceName").toString();
-        String platformName = this.capabilities.getCapability("platformName").toString();
-        switch (platformName) {
-            case "iOS":
-                //todo
-                break;
-            case "Android":
-            default:
-                this.androidEmulator.start(deviceName);
-                break;
-        }
-    }
-
-    public void stopSimulator() {
-        String platformName = this.capabilities.getCapability("platformName").toString();
-        switch (platformName) {
-            case "iOS":
-                //todo
-                break;
-            case "Android":
-            default:
-                this.androidEmulator.stop();
-                break;
-        }
     }
 }
