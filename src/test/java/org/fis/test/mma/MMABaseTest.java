711e736e1618be014157a52e910df99de4285af0
Willy De la Cruz Sierra <will.delacruz.sierra@gmail.com>
2020-01-15 17:27:01 -0500

    Adding DriverManager wrapper class

--- src/test/java/org/fis/test/mma/MMABaseTest.java
+++ src/test/java/org/fis/test/mma/MMABaseTest.java
@@ -1,14 +1,9 @@
 package org.fis.test.mma;
 
 import org.fis.framework.BaseTest;
-import org.testng.annotations.BeforeMethod;
+import org.fis.test.support.pages.LoginPage;
 import org.testng.annotations.Test;
 
 public abstract class MMABaseTest extends BaseTest {
-
-    @Test
-    public void test1(){
-        LoginPage loginPage = new LoginPage(this.driverManager);
-    }
 
 }
